64f9e41fa301fea71eee6566d76aa3cbb2460991
Erica Pisani <pisani.erica@gmail.com>
2022-07-20 15:24:53 -0400

    test: debug logging

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
+  console.log('REQUEST RECEIVED', req)
   const { nextUrl: url, geo } = req
   url.searchParams.set('country', geo.country)
   return NextResponse.rewrite(url)
