3712d2934516f1ea9737a4ec782ef91c9e06a0cc
Erica Pisani <pisani.erica@gmail.com>
2022-07-21 09:34:29 -0400

    Go back to original implementation as it works

--- middleware.js
+++ middleware.js
@@ -1,8 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
-  console.log('REQUEST RECEIVED', req)
   const { nextUrl: url, geo } = req
   url.searchParams.set('country', geo.country)
-  return NextResponse.redirect(url)
+  return NextResponse.rewrite(url)
 }
