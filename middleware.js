dfce2d8f6135e6f019327aa6af08be30f4ecdf50
Gualter <gualterandre@gmail.com>
2022-11-30 10:56:19 +0000

    added MiddlewareRequest example

--- middleware.js
+++ middleware.js
@@ -1,8 +1,22 @@
-import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
+import { MiddlewareRequest } from '@netlify/next'
 
-export async function middleware(req) {
-  const { nextUrl: url, geo } = req
+export async function middleware(req: NextRequest) {
+  const { geo } = req
+  const country = geo?.country || ''
+
+  const request = new MiddlewareRequest(req)
+  const response = await request.next()
+
   console.log(`Processing request from ${geo.country}`)
-  url.searchParams.set('country', geo.country)
-  return NextResponse.rewrite(url)
+
+  if (country === 'PT') {
+    const message =
+      'Hello Portugal! Use promo code COUNTRY50 for 50% off your order!'
+
+    response.replaceText('#banner', message)
+    response.setPageProp('banner', message)
+  }
+
+  return response
 }
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import countries from '../lib/countries'
 
-export const getServerSideProps = ({ query }) => ({
-  props: query,
-})
+export async function getStaticProps() {
+  return {
+    props: {
+      banner: 'Hello world!',
+    },
+  }
+}
 
-export default function Home({ country }) {
+export default function Home({ banner }) {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Next.js Geolocation in Edge Functions</title>
-        <meta
-          name="description"
-          content="A simple starter template demonstrating Geolocation in Edge Functions"
-        />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Next.js Geolocation in Edge Functions</h1>
-        <p className={styles.description}>
-          {country && country !== 'undefined' ? (
-            <>
-              <span className={styles.flag}>
-                <Image
-                  alt={`${country} flag`}
-                  src={`https://flagcdn.com/96x72/${country.toLowerCase()}.png`}
-                  width={32}
-                  height={24}
-                />
-              </span>
-              <span className={styles.welcome}>
-                Welcome from {countries.find((c) => c.code === country).name}
-              </span>
-            </>
-          ) : (
-            `We're unable to find you!`
-          )}
-        </p>
-      </main>
+      <div id="banner" style={{ border: 'solid', padding: '16px' }}>
+        {banner}
+      </div>
+      <div id="content">Common content would be here</div>
     </div>
   )
 }
