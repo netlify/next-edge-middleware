6a724be503a73fb2827a471277517beb6666c192
Gualter <gualterandre@gmail.com>
2022-11-30 10:23:53 +0000

    added console.log on middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
+  console.log(`Processing request from ${geo.country}`)
   const { nextUrl: url, geo } = req
   url.searchParams.set('country', geo.country)
   return NextResponse.rewrite(url)
