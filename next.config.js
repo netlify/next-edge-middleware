8d52a2db6bfe1e7bb94823ae0c7366a0da91ae21
Gualter <gualterandre@gmail.com>
2023-03-14 19:27:10 +0000

    added image

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['flagcdn.com', 'images.unsplash.com'],
+    domains: ['flagcdn.com', 'assets.ctfassets.net'],
     formats: ['image/avif'],
   },
 }
--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,9 @@ export default function Home({ banner }) {
       <p id="content">Here's an image for you!</p>
       <div className="image-wrapper">
         <Image
-          width={1000}
-          height={1000}
-          src="https://images.unsplash.com/photo-1513735492246-483525079686"
+          width={1500}
+          height={1500}
+          src="https://assets.ctfassets.net/bmlje59etcpz/2gqHEDgkbhV2hM38iEqC7R/a226a75847f69645dda543d54e176967/photo-1513735492246-483525079686.avif"
           alt="Picture of Porto"
         />
       </div>
