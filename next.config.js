8415eed196861db5d0c4504212304d0fb00bba75
Gualter <gualterandre@gmail.com>
2023-03-14 19:37:59 +0000

    added image

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['flagcdn.com', 'assets.ctfassets.net'],
-    formats: ['image/avif', 'image/webp'],
+    formats: ['image/webp'],
   },
 }
 
