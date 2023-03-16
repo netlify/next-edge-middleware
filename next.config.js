007c3c299b86a4d0bfc2f1086788ae9637888d51
Gualter <gualterandre@gmail.com>
2023-03-16 14:41:08 +0000

    testing

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['flagcdn.com', 'images.ctfassets.net'],
+    domains: ['flagcdn.com', 'images.ctfassets.net', 'app.thefeedfactory.nl'],
     formats: ['image/avif', 'image/webp'],
   },
 }
