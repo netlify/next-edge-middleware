3018b08bd001b7ba8d4096317a190a1f3cbf4635
Gualter <gualterandre@gmail.com>
2023-07-24 10:10:08 +0100

    added EF

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,3 +1,8 @@
-export default () => new Response("Hello world");
+export default () =>
+  new Response(
+    'this is a response that renders on every root path, except if it has anything but an alphanumeric, underscore and dashes. Also, it should not be executed if there are any subpaths.'
+  )
 
-export const config = { path: "/hello" };
+export const config = {
+  path: '^/[*@([a-zA-Z0-9_-])]*(/|/*[^/]*[a-zA-Z0-9_-]*)',
+}
