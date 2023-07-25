45e2b03a440efc2c858fb1dcbcf44c499543e3e9
Gualter Augusto <35470944+gualterandre@users.noreply.github.com>
2023-07-25 10:01:29 +0100

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -2,7 +2,3 @@ export default () =>
   new Response(
     'this is a response that renders on every root path, except if it has anything but an alphanumeric, underscore and dashes. Also, it should not be executed if there are any subpaths.'
   )
-
-export const config = {
-  pattern: '^/[*@([a-zA-Z0-9_-])]*(/|/*[^/]*[a-zA-Z0-9_-]*)',
-}
