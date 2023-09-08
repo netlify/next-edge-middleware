29aa0063270515a9ebea05e74085c67f5512b1db
Gualter Augusto <35470944+gualterandre@users.noreply.github.com>
2023-09-08 09:40:15 +0100

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,4 +1,4 @@
 export default () =>
   new Response(
-    'this is a response that renders on every root path, except if it has anything but an alphanumeric, underscore and dashes. Also, it should not be executed if there are any subpaths.'
+    'hello world!'
   )
